01d53763fcdd256a50386027b57e87db3923726c
Marmin <marmin.dev@gmail.com>
2023-03-20 02:04:24 +0900

    add markdown and anker

--- projects/mysite/pybo/templatetags/pybo_filter.py
+++ projects/mysite/pybo/templatetags/pybo_filter.py
@@ -1,7 +1,13 @@
 from django import template
-
+from django.utils.safestring import mark_safe
+import markdown
 register = template.Library()
 
 @register.filter
 def sub(value, arg):
     return value - arg
+
+@register.filter()
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
