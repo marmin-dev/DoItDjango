b4162c91a4acad50bc253a3673e8f81dbe54a231
Marmin <marmin.dev@gmail.com>
2023-03-21 13:40:23 +0900

    back

--- projects/mysite/pybo/urls.py
+++ projects/mysite/pybo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import question_views, base_views, answer_views, comment_views, vote_views, profile_views
+from .views import question_views, base_views, answer_views, comment_views, vote_views
 
 app_name = 'pybo'
 
@@ -40,6 +40,6 @@ urlpatterns = [
     path('vote/question/<int:question_id>/', vote_views.vote_question, name="vote_question"),
     path('vote/answer/<int:answer_id>/', vote_views.vote_answer, name="vote_answer"),
 
-    # profile_views.py
-    path('profile/<str:username>/',profile_views.get_profile,name="get_profile")
+
+
 ]
