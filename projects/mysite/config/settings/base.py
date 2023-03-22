ab2427c517d125308160ab59d049d72eab933717
Marmin <marmin.dev@gmail.com>
2023-03-22 11:36:46 +0900

    before prod

--- projects/mysite/config/settings/base.py
+++ projects/mysite/config/settings/base.py
@@ -15,7 +15,7 @@ import os, json
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # get secret key
 secret_file = os.path.join(BASE_DIR, 'secret.json')
