e2e0a8c0be41f667041e6a0affd11dde9f823b51
Marmin <marmin.dev@gmail.com>
2023-03-22 15:47:52 +0900

    add logging

--- projects/mysite/config/settings/base.py
+++ projects/mysite/config/settings/base.py
@@ -192,3 +192,66 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+
+DEFAULT_LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'django.server': {
+            '()': 'django.utils.log.ServerFormatter',
+            'format': '[{server_time}] {message}',
+            'style': '{',
+        },
+        'standard':{
+          'format':'%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'handlers':{
+            'level':'INFO',
+            'filters':['require_debug_false'],
+            'class':'logging.handlers.RotatingFilterHandler',
+            'filename':1024*1024*5,
+            'backupCount':5,
+            'formatter':'standard',
+        },
+        'django.server': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'django.server',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'mail_admins','file'],
+            'level': 'INFO',
+        },
+        'django.server': {
+            'handlers': ['django.server'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'pybo':{
+            'handlers':['console','file'],
+            'level':'INFO',
+        },
+    },
+}
--- projects/mysite/pybo/views/base_views.py
+++ projects/mysite/pybo/views/base_views.py
@@ -3,12 +3,16 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import Q,Count
 
 from ..models import Question,Answer
+import logging
+
+logger=logging.getLogger('pybo')
 
 
 def index(request,category="all"):
     """
     pybo 목록 출력
     """
+    logger.info("INFO 레벨로 출력")
     page = request.GET.get('page','1') # 페이지
     kw = request.GET.get('kw','')
     so = request.GET.get('so','recent')
